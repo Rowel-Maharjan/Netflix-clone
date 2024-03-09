db3c9a9901664adf45cd438edcfc13d82bcee21d
Rowel Maharjan <rowelmhj@gmail.com>
2024-03-09 10:21:53 +0545

    Added js

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Hide all showfull elements initially
-    let showfulls = document.querySelectorAll('.showfull');
-    showfulls.forEach(showfull => {
-        showfull.style.display = "none";
-    });
-});
+// function toggleHide(clickedElement) {
+//     let showfulls = document.querySelectorAll('.showfull');
+    
+//     showfulls.forEach(showfull => {
+//         if (showfull !== clickedElement.nextElementSibling) {
+//             showfull.style.display = "none";
+//         }
+//     });
+
+//     let showfull = clickedElement.nextElementSibling;
+
+//     if (showfull.style.display !== "none") {
+//         showfull.style.display = "none";
+//     } else {
+//         showfull.style.display = "block";
+//     }
+//     let icon = clickedElement.querySelector('.icon img');
+//     if (showfull.style.display === "none") {
+//         icon.src = "\\assets\\images\\addition.svg";
+//     } else {
+//         icon.src = "\\assets\\images\\multiply.svg"; 
+//     }
+// }
 
 function toggleHide(clickedElement) {
     let showfulls = document.querySelectorAll('.showfull');
-    
+
     showfulls.forEach(showfull => {
         if (showfull !== clickedElement.nextElementSibling) {
-            showfull.style.display = "none";
+            showfull.classList.remove('active');
         }
     });
 
     let showfull = clickedElement.nextElementSibling;
-
-    if (showfull.style.display !== "none") {
-        showfull.style.display = "none";
-    } else {
-        showfull.style.display = "block";
-    }
-
-    // Change the image source based on the display status
+    showfull.classList.toggle('active');
+    
     let icon = clickedElement.querySelector('.icon img');
-    if (showfull.style.display === "none") {
-        icon.src = "\\assets\\images\\addition.svg";
-    } else {
+    if (showfull.classList.contains('active')) {
         icon.src = "\\assets\\images\\multiply.svg"; 
+    } else {
+        icon.src = "\\assets\\images\\addition.svg";
     }
 }
+
